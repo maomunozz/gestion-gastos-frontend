adefbb47a5e2e942f3cf61cb7f6a35ddae78b9bd
Hector Mauricio Muñoz Ordoñez <dev.mauricio.munoz@gmail.com>
2025-03-06 17:55:56 -0500

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,1 @@
-// Obtener el token del localStorage
-export function getToken() {
-  return localStorage.getItem("token");
-}
 
-// Establecer el token en localStorage
-export function setToken(token) {
-  localStorage.setItem("token", token);
-}
-
-// Eliminar el token de localStorage (cuando el usuario cierre sesión)
-export function clearToken() {
-  localStorage.removeItem("token");
-}
